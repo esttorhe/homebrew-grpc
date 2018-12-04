b0a00376f2d5ef9c9b5e633802d922885bd66681
Esteban Torres <me@estebantorr.es>
2018-12-04 23:06:30 +0100

    Point to head of «almost» master

--- Formula/grpc-swift.rb
+++ Formula/grpc-swift.rb
@@ -1,7 +1,7 @@
  class GrpcSwift < Formula
   desc "The Swift gRPC implementation. HTTP/2 based RPC"
   homepage "https://grpc.io"
-  url "https://github.com/grpc/grpc-swift.git", :tag => "0.6.0", :revision => "dca553544209d45ccf3ef782bd0371fda63f1006"
+  url "https://github.com/grpc/grpc-swift.git", :revision => "1d422fc31cd08db7d85e4137b5dc7401c050f637"
 
   def install
     system("swift build --disable-sandbox -Xcc -ISources/BoringSSL/include -Xlinker -lz")
